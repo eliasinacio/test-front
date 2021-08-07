5e04264f6c6d7531ed3de6498d2b6a025855c668
Elias Inacio <0.elias.inacio@gmail.com>
2021-08-07 20:28:02 -0300

    finished.

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,22 +1,13 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import '../styles/search.scss'
 
 const SearchBar = () => {
   const [ input, setInput ] = useState('')
 
-  function changeInput (event) {
+  function search (event) {
     event.preventDefault();
     setInput(event.target.value);
 
-    if (event.target.value === '') {
-      for (let i=0; i<9; i++) {
-        const tableRow = document.querySelector('tbody').childNodes[i];
-        tableRow.style.display = ''
-      }
-    }
-  }
-
-  function search () {
     const tableBody = document.querySelector('tbody');
 
     for (let i=0; i<9; i++) {
@@ -26,10 +17,16 @@ const SearchBar = () => {
       let tableRowText = tableRow.textContent.toLowerCase();
       tableRowText = tableRowText.normalize('NFD').replace(/[\u0300-\u036f]/g, "");
 
-      const inputText = input.normalize('NFD').replace(/[\u0300-\u036f]/g, "");
+      const inputText = event.target.value.normalize('NFD').replace(/[\u0300-\u036f]/g, "");
       
-      if (tableRowText.search(inputText.toLowerCase()) === -1) {
-        tableRow.style.display = 'none'
+      try {
+        if (tableRowText.search(inputText.toLowerCase()) === -1) {
+          tableRow.style.display = 'none'
+        } else {
+          tableRow.style.display = ''
+        }
+      } catch (error) {
+        console.log(new Error("Erro ao buscar dado."))
       }
     }
   }
@@ -40,10 +37,9 @@ const SearchBar = () => {
         type="text" 
         placeholder="Pesquisar" 
         value={input}
-        onChange={() => changeInput(event)}/>
-      <button onClick={search}>
-        <img src="https://img.icons8.com/material-outlined/28/999999/search--v1.png" alt="search icon, click to search"/>
-      </button>
+        onChange={() => search(event)}/>
+
+      <img src="https://img.icons8.com/material-outlined/28/999999/search--v1.png" alt="search icon, click to search"/>
     </label>
   )
 }
